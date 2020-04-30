21c34ac6a3665ff85f18a914c31059eb99026b38
Samuel Sutch <sam@sutch.net>
2020-04-30 00:28:52 -0700

    switch to setuptools; build walletkitcore as a shared library to cut down on compile time

--- WalletKitPython/setup.py
+++ WalletKitPython/setup.py
@@ -3,8 +3,7 @@ import itertools
 import platform
 from os import path
 from pathlib import Path
-from setuptools import Extension
-from distutils.core import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 LIBRARIES = ["resolv"]
@@ -20,6 +19,15 @@ CORE_SRC_FILES = list(map(str, itertools.chain(
     Path(path.join(CORE_ROOT, 'src')).rglob('*.c'),
     Path(path.join(CORE_ROOT, 'vendor', 'ed25519')).rglob('*.c')
 )))
+CORE_COMPILE_ARGS = [
+    "-Wall",
+    "-Wconversion",
+    "-Wsign-conversion",
+    "-Wparentheses",
+    "-Wswitch",
+    "-Wno-implicit-int-conversion",
+    "-Wno-missing-braces",
+]
 CYTHON_FILES = list(map(str, Path(path.join(HERE, 'walletkit')).glob('*.pyx')))
 CYTHON_INCLUDE_DIRS = [path.join(HERE, 'walletkit')]
 INCLUDE_DIRS = [
@@ -28,20 +36,6 @@ INCLUDE_DIRS = [
     path.join(CORE_ROOT, 'vendor'),
     path.join(CORE_ROOT, 'vendor', 'secp256k1'),
 ]
-EXTENSIONS = [Extension(
-    'native', CYTHON_FILES + CORE_SRC_FILES,
-    include_dirs=INCLUDE_DIRS,
-    libraries=LIBRARIES,
-    extra_compile_args=[
-        "-Wall",
-        "-Wconversion",
-        "-Wsign-conversion",
-        "-Wparentheses",
-        "-Wswitch",
-        "-Wno-implicit-int-conversion",
-        "-Wno-missing-braces",
-    ]
-)]
 
 setup(
     name='walletkit',
@@ -51,12 +45,28 @@ setup(
     description="A wrapper around the WalletKit library",
     license="MIT",
     url="https://github.com/blockset-corp/walletkit",
-    packages=['walletkit'],
+    packages=['walletkit', 'walletkit.wordlists'],
+    setup_requires=[
+        'setuptools>=41.6.0',
+        'Cython>=0.29.14',
+    ],
     ext_package='walletkit',
-    ext_modules=cythonize(
-        EXTENSIONS,
+    ext_modules=cythonize([
+        Extension(
+            'native', CYTHON_FILES,
+            include_dirs=INCLUDE_DIRS,
+            libraries=["walletkitcore"] + LIBRARIES
+        )],
         include_path=CYTHON_INCLUDE_DIRS,
         language_level=3
     ),
+    libraries=[
+        ['walletkitcore', {
+            'sources': CORE_SRC_FILES,
+            'include_dirs': INCLUDE_DIRS,
+            'libraries': LIBRARIES,
+            'extra_compile_args': CORE_COMPILE_ARGS
+        }]
+    ],
     zip_safe=False
 )
